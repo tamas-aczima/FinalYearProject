993c79eff13593c367d436703c9495a21da87b4d
Tamas Aczima <tamas.aczima@bossastudios.com>
2018-02-06 10:56:03 +0000

    Added VR

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.VR;
 
 public class CameraController : MonoBehaviour {
 
     private GameObject activeCamera;
     [SerializeField] private GameObject firstPersonCamera;
     [SerializeField] private GameObject thirdPersonCamera;
+    [SerializeField] private GameObject vrCamera;
     private bool cameraChanged = false;
+    private bool isVR;
 
-	// Use this for initialization
-	void Start () {
-        activeCamera = thirdPersonCamera;
+    void Start () {
+        isVR = VRDevice.isPresent;
+
+        if (isVR)
+        {
+            activeCamera = vrCamera;
+        }
+        else
+        {
+            activeCamera = thirdPersonCamera;
+        }
+
+        activeCamera.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        ChangeCamera();
+    }
+
+    private void ChangeCamera()
+    {
+        if (isVR) return;
+
         if (activeCamera == firstPersonCamera)
         {
             firstPersonCamera.SetActive(true);
@@ -27,7 +45,7 @@ public class CameraController : MonoBehaviour {
             firstPersonCamera.SetActive(false);
         }
 
-        if (!gameObject.GetComponent<YostSkeletonRig>().GetJoyStickButtonDown(YostSkeletalAPI.YOST_SKELETON_JOYSTICK_BUTTON.YOST_SKELETON_LEFT_X_BUTTON))
+        if (gameObject.GetComponent<YostSkeletonRig>().GetJoyStickButtonUp(YostSkeletalAPI.YOST_SKELETON_JOYSTICK_BUTTON.YOST_SKELETON_LEFT_X_BUTTON))
         {
             cameraChanged = false;
         }
